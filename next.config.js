75e46cc5a013f21e342e0a62f6424f88fe55dbc4
Emre <emreturkan10@gmail.com>
2023-11-12 12:10:55 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**',
-			},
-		],
-	},
-}
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  headers: [
+    {
+      source: "/api/:path*",
+      headers: [
+        {
+          key: "Access-Control-Allow-Origin",
+          value: "*",
+        },
+      ],
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
