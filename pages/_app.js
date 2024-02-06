c74b94e9542f324471823971dd29bbe43b29fc87
emreturkan <emreturkan10@gmail.com>
2024-02-06 17:40:07 +0300

    add: vercel analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,20 @@
-import Layout from '@/components/layout/layout'
-import {Metadata} from '@/lib/metadata'
-import '@/styles/globals.css'
-import 'react-loading-skeleton/dist/skeleton.css'
-export default function App({Component, pageProps}) {
-	return (
-		<>
-			<Metadata
-				title="Emre Turkan"
-				content=" Emre Turkan Kişisel Web Sitesi"
-				desc="Emre Turkan Kişisel Web Sitesi"
-			/>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</>
-	)
+import Layout from "@/components/layout/layout";
+import { Analytics } from "@vercel/analytics/react";
+import { Metadata } from "@/lib/metadata";
+import "@/styles/globals.css";
+import "react-loading-skeleton/dist/skeleton.css";
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Metadata
+        title="Emre Turkan"
+        content=" Emre Turkan Kişisel Web Sitesi"
+        desc="Emre Turkan Kişisel Web Sitesi"
+      />
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </>
+  );
 }
